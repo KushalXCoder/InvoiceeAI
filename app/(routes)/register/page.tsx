a918a763f72beed915cfcf125911bc730b2e2304
Kushal Rathod <kushalrathod044@gmail.com>
2025-08-26 12:10:30 +0530

    Changes

--- app/(routes)/register/page.tsx
+++ app/(routes)/register/page.tsx
@@ -1,6 +1,6 @@
-import Form from '@/app/components/Form';
+// import Form from '@/app/components/Form';
 import GoogleButton from '@/app/components/GoogleButton';
-import Link from 'next/link';
+// import Link from 'next/link';
 import React from 'react';
 
 const SignInPage = () => {
@@ -10,10 +10,10 @@ const SignInPage = () => {
         <div className="sign-in-box h-fit w-fit rounded-lg shadow-2xl backdrop-blur-2xl flex flex-col items-center px-5 py-5 gap-5 bg-white max-sm:px-2">
             <div className="heading flex flex-col items-center">
                 <h1 className='font-facultyGlyphic text-lg'>Sign Up to InvoiceeAI</h1>
-                <p className='text-gray-500 font-poppins text-sm text-center w-3/4 max-sm:w-full max-sm:mt-2'>Looks like its your first time, fill details to continue</p>
+                <p className='text-gray-500 font-poppins text-sm text-center w-3/4 max-sm:w-full max-sm:mt-2 mt-2'>Looks like its your first time, fill details to continue</p>
             </div>
             <GoogleButton/>
-            <div className="or flex justify-center items-center w-full gap-4">
+            {/* <div className="or flex justify-center items-center w-full gap-4">
                 <div className="line h-[1px] w-1/3 bg-black"></div>
                 <p className='font-facultyGlyphic'>or</p>
                 <div className="line h-[1px] w-1/3 bg-black"></div>
@@ -21,8 +21,8 @@ const SignInPage = () => {
             <Form type="Signup"/>
             <p className='font-facultyGlyphic max-sm:text-[15px]'>Already have an account ? 
                 <Link href="/login" className='underline hover:text-blue-600 transition-colors'> Sign In </Link>
-            </p>
-            <p className='font-facultyGlyphic border px-5 py-1 bg-blue-950 text-white rounded-lg'>Powered by InvoiceeAI</p>
+            </p> */}
+            <p className='font-facultyGlyphic border px-5 py-1 bg-blue-950 text-white rounded-lg mt-2'>Powered by InvoiceeAI</p>
         </div>
     </div>
   )
